bad266c404895cc3424eccdcfe1e61f894c6331b
Atmojomojo <rotastisch@gmail.com>
2025-01-21 17:37:19 +0100

    Pawns are in the game and Bats have been updated
    Bats now have inconsistency in the y axis and have a smoke particle coming from them.
    
    The target script also has a array of transforms in here you can put emptys and that is the point where the bats spawn you can put as many spawns and per bat it will choose a random spawn

--- Assets/Scripts/GoToTarget.cs
+++ Assets/Scripts/GoToTarget.cs
@@ -4,14 +4,24 @@ public class GoToTarget : MonoBehaviour
 {
     public Transform target; // The target the object is moving towards
     public float moveSpeedMin, moveSpeedMax; // Speed of the movement
-    public float rotationSpeedMin,rotationSpeedMax; // Maximum speed of rotation in degrees per second
+    public float rotationSpeedMin, rotationSpeedMax; // Maximum speed of rotation in degrees per second
+
+    // New variables for height oscillation
+    public float oscillationStrength = 0.5f; // Random height variation strength
+    public float oscillationSpeed = 2f;     // Speed of vertical oscillation
 
     private float moveSpeed, rotationSpeed;
+    private float oscillationOffset;
+
     private void Start()
     {
         moveSpeed = Random.Range(moveSpeedMin, moveSpeedMax);
         rotationSpeed = Random.Range(rotationSpeedMin, rotationSpeedMax);
+
+        // Randomize initial oscillation offset for each bat
+        oscillationOffset = Random.Range(0f, Mathf.PI * 2f);
     }
+
     private void Update()
     {
         MoveAndRotate();
@@ -25,6 +35,12 @@ public class GoToTarget : MonoBehaviour
 
         // Calculate the direction towards the target
         Vector3 direction = target.position - transform.position;
+
+        // Add oscillation to the vertical (Y-axis) direction
+        direction.y += Mathf.Sin(Time.time * oscillationSpeed + oscillationOffset) * oscillationStrength;
+
+        // Normalize the direction vector
+        direction.Normalize();
 
         // Rotate smoothly towards the target
         Quaternion targetRotation = Quaternion.LookRotation(direction);
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -7,9 +7,17 @@ public class Target : MonoBehaviour
     public GameObject agent;
     public int numberOfAgents;
     public float timeBetweenSpawns;
+
+    public Transform[] spawns;
     // Start is called before the first frame update
     void Start()
     {
+        if (spawns.Length == 0)
+        {
+            spawns = new Transform[1];
+            spawns[0] = transform;
+            print("Working");
+        }
         StartCoroutine(SpawnAgents());
     }
 
@@ -17,7 +25,8 @@ public class Target : MonoBehaviour
     {
         while (numberOfAgents != 0)
         {
-            GoToTarget spawned = Instantiate(agent.GetComponent<GoToTarget>());
+            int randomSpawn = Random.Range(0, spawns.Length);
+            GoToTarget spawned = Instantiate(agent.GetComponent<GoToTarget>(), spawns[randomSpawn].position, spawns[randomSpawn].rotation);
             spawned.target = transform;
             numberOfAgents -= 1;
             yield return new WaitForSeconds(timeBetweenSpawns);
